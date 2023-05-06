a74a8d3dc3b9d6545ce5c4e3521ab43b9e639998
Rafael da Silveira <rafael.dasilveira@auto1.com>
2023-05-06 18:41:01 +0200

    remove test

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 import os
-import unittest
 
 from flask_script import Manager
 
@@ -11,13 +10,6 @@ manager = Manager(app)
 
 def run():
     manager.run()
-
-def test():
-    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 if __name__ == '__main__':
     manager.run()
